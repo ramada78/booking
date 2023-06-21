88c1e9be576d41a2d2f16125a0d2e12d362e7e99
ramada78 <137193883+ramada78@users.noreply.github.com>
2023-06-21 13:31:08 +0300

    Program.cs

--- HotelBookingAPI/Program.cs
+++ HotelBookingAPI/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddDbContext<ApiContext>
     (opt => opt.UseInMemoryDatabase("BookingDb"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
